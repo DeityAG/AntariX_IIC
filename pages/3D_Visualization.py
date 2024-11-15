5a5aad39e599746113e56e1e613b174d9d8ea47c
Aditya Kumar Guru <adityaguru20@gmail.com>
2024-11-15 18:54:02 +0530

    Update 3D_Visualization.py

--- pages/3D_Visualization.py
+++ pages/3D_Visualization.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
 st.title("3D Visualization")
-st.write("This page will feature a 3D interactive plot for satellite orbits. (Feature under development)")
+st.write("Interactive 3D Satellite Orbit Visualization with Cesium.js")
+
+# Path to the HTML file
+html_file_path = "3D_Visualization.html"
+
+# Embed the HTML file in an iframe
+st.components.v1.html(
+    f"""
+    <iframe src="{html_file_path}" width="100%" height="600px" style="border:none;"></iframe>
+    """,
+    height=600,
+)
