6a3429d63c1e93928aea373a0e85c6a086871761
Aditya Kumar Guru <adityaguru20@gmail.com>
2024-11-15 18:14:55 +0530

    Create Details_References.py

---
+++ pages/Details_References.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Details and References")
+st.write("Learn more about satellite orbit prediction and the underlying technologies.")
+
+st.subheader("References")
+st.markdown("""
+- **SGP4 Orbit Prediction Model**: [Link to Documentation](https://celestrak.com/NORAD/documentation/)
+- **Two-Line Element (TLE) Format**: [Wikipedia Article](https://en.wikipedia.org/wiki/Two-line_element_set)
+- **Low Earth Orbit (LEO) Satellites**: [NASA's Guide on LEO](https://www.nasa.gov/)
+- **Streamlit Documentation**: [Streamlit Docs](https://docs.streamlit.io/)
+""")
+
+st.subheader("Resources")
+st.markdown("""
+- **CelesTrak TLE Data**: [CelesTrak](https://www.celestrak.com/)
+- **Lottie Animations for Apps**: [LottieFiles](https://lottiefiles.com/)
+- **Plotly Graphing Library**: [Plotly Docs](https://plotly.com/python/)
+""")
